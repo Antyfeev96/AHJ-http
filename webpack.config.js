131a1cab42b6d354f8da1c0c08b67eda6e0b1259
Dmitri Antyfeev <60137521+Antyfeev96@users.noreply.github.com>
2021-03-09 14:45:42 +0300

    addButton element added

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ const webpack = require('webpack');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ESLintPlugin = require('eslint-webpack-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
+// const CopyPlugin = require('copy-webpack-plugin');
 
 /*
  * We've enabled HtmlWebpackPlugin for you! This generates a html
@@ -26,11 +26,11 @@ const CopyPlugin = require('copy-webpack-plugin');
  */
 
 module.exports = {
-  entry: './src/index.js',
+  // entry: './src/index.js',
   mode: 'development',
-  output: {
-    filename: 'bundle.js',
-  },
+  // output: {
+  //   filename: 'bundle.js',
+  // },
   plugins: [
     new webpack.ProgressPlugin(),
     new MiniCssExtractPlugin({ filename: 'main.[contenthash].css' }),
@@ -38,11 +38,11 @@ module.exports = {
       template: 'index.html',
     }),
     new ESLintPlugin(),
-    new CopyPlugin({
-      patterns: [
-        { from: 'src/images', to: 'src/images' },
-      ],
-    }),
+    // new CopyPlugin({
+    //   patterns: [
+    //     { from: 'src/images', to: 'src/images' },
+    //   ],
+    // }),
   ],
 
   module: {
