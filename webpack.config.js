168f3193b4b607d0572343f36d148c439d644ae6
Dmitri Antyfeev <60137521+Antyfeev96@users.noreply.github.com>
2021-03-10 13:44:51 +0300

    Config fixed

--- webpack.config.js
+++ webpack.config.js
@@ -26,10 +26,13 @@ const CopyPlugin = require('copy-webpack-plugin');
  */
 
 module.exports = {
-  entry: '../src/index.js',
+  entry: {
+    main: path.resolve(__dirname, './src/index.js'),
+  },
   mode: 'development',
   output: {
-    filename: 'bundle.js',
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].bundle.js',
   },
   plugins: [
     new webpack.ProgressPlugin(),
