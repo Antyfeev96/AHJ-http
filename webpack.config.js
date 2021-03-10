49f5f7f3da2a0371bd8168de2100c395d73c1768
Dmitri Antyfeev <60137521+Antyfeev96@users.noreply.github.com>
2021-03-10 14:50:04 +0300

    Trying to fix config

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ const webpack = require('webpack');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ESLintPlugin = require('eslint-webpack-plugin');
-// const CopyPlugin = require('copy-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 /*
  * We've enabled HtmlWebpackPlugin for you! This generates a html
@@ -39,11 +39,11 @@ module.exports = {
       template: 'index.html',
     }),
     new ESLintPlugin(),
-    // new CopyPlugin({
-    //   patterns: [
-    //     { from: 'src/images', to: 'src/images' },
-    //   ],
-    // }),
+    new CopyPlugin({
+      patterns: [
+        { from: 'src', to: 'src' },
+      ],
+    }),
   ],
 
   module: {
